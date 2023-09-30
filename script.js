4c23e89f5453426c2caa280d4dce0f70125360ff
Ram18kumar <134294896+Ram18kumar@users.noreply.github.com>
2023-09-30 13:17:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const arr=[3,4,5,6];
+const n=arr.map(item=>item-2);
+console.log(n);
+//reduce
+function sum (a,b){
+return a+b;
+}
+const arr1=[2,3,4,5];
+const m=arr1.reduce(sum);
+console.log(m);
